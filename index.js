cff6092c79d09d335607596f71fcf0accb4054c5
Noda Shimpei <noda.sin@gmail.com>
2015-11-10 13:59:46 +0900

    Added flow for uncaughtException

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function printError(err) {
   console.error('[' + new Date().toUTCString() + '] ' + err);
 }
 
+process.on('uncaughtException', function (err) {
+  printError(err);
+});
+
 if (config.options.ssl) {
   config.options.ssl = {
     key: fs.readFileSync(path.join(__dirname, config.options.ssl.key)),
